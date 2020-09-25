df1fe41428c111c2d42717daa1d63e3fcd0123b0
muhismas <58177087+muhismas@users.noreply.github.com>
2020-09-25 20:52:19 +0300

    Update Paaohjelma.java

--- Paaohjelma.java
+++ Paaohjelma.java
@@ -2,7 +2,7 @@
 public class Paaohjelma {
 
     public static void main(String[] args) {
-        // tee tänne testipääohjelmia
+        
         Maksukortti pekanKortti = new Maksukortti(10);
 
         System.out.println("rahaa " + pekanKortti.saldo());
